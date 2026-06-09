b06637d8f569d77a31b2f11f9171a31807686647
Chrystian Dybas <dybas.chrys@gmail.com>
2026-06-09 20:18:57 +0100

    gets dir

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use std::env;
+use std::io::Result;
+use std::{fs, io};
 
 
 
-fn main() {
+
+
+fn main() -> io::Result<()> {
+
+
+
 
     let args: Vec<String> = env::args().collect();
     
     let file_path = &args[1];
 
-    println!("File path - {file_path}");
+
+    let mut entries = fs::read_dir(file_path)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>>>()?;
+
+    entries.sort();
+    
+
+
+    Ok(())
+
 
 
 }
