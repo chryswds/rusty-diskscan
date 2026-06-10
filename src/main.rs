95e5fccf2dfb6c0c55bda79d4ec75747a65f118c
Chrystian Dybas <dybas.chrys@gmail.com>
2026-06-10 21:06:55 +0100

    even more idiomatic clean up

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,10 @@ fn dir_size(dir_path: &Path) -> io::Result<u64> {
     let mut total_size: u64 = 0;
     for entry in &entries {
         let metadata = fs::metadata(entry)?;
+        
+
         if metadata.is_dir() {
-            let folder_size = dir_size(entry)?;
+            let folder_size = dir_size(entry).unwrap_or(0);
             total_size += folder_size;
         } else {
             let size = metadata.len();
@@ -37,34 +39,34 @@ fn dir_size(dir_path: &Path) -> io::Result<u64> {
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let dir_path_str = &args[1];
-    let dir_path_path = Path::new(&dir_path_str);
-    println!("{:?}", dir_path_path);
 
-    let entries = fs::read_dir(dir_path_path)?
+    let path = Path::new(&args[1]);
+    println!("{}", path.display());
+
+    let entries = fs::read_dir(path)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>>>()?;
 
-    let mut v: Vec<(u64, &PathBuf)> = Vec::new();
+    let mut entries_by_size: Vec<(u64, &PathBuf)> = Vec::new();
 
 
     for entry in &entries {
         let metadata = fs::metadata(entry)?;
 
         if metadata.is_dir() {
-            let folder_size = dir_size(entry)?;
-            v.push((folder_size, entry));
+            let folder_size = dir_size(entry).unwrap_or(0);
+            entries_by_size.push((folder_size, entry));
         } else {
             let file_size = metadata.len();
-            v.push((file_size, entry));
+            entries_by_size.push((file_size, entry));
         }
     }
 
-    v.sort_by(|a, b| b.cmp(a));
-   
+    
+    entries_by_size.sort_by_key(|(s, _)| std::cmp::Reverse(*s));
 
     let mut total_size: u64 = 0;
-    for (size, path) in &v {
+    for (size, path) in &entries_by_size {
         println!("{} ---- {}", path.display(), readable_size(*size));
         total_size += size;
     
