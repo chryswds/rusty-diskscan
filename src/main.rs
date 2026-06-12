da6d372c09dbadc088a78bab07f9fd7ee79dfbb2
Chrystian Dybas <dybas.chrys@gmail.com>
2026-06-12 11:40:36 +0100

    puts dir check into its own fn with error handling returning the size regardless

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::Result;
 use std::path::{Path, PathBuf};
 use std::{fs, io};
 
+
 fn readable_size(bytes: u64) -> String {
     let base = 1024;
     let float_value = bytes as f64;
@@ -17,29 +18,37 @@ fn readable_size(bytes: u64) -> String {
     }
 }
 
+fn entry_size(entry: &Path) -> u64 {
+    let outcome = fs::metadata(entry);
+
+    let metadata = match outcome {
+        Ok(m) => m,
+        Err(_) => return 0,
+    };
+    let mut total_size = 0;
+    if metadata.is_dir() {
+        let folder_size = dir_size(entry).unwrap_or(0);
+        total_size += folder_size;
+    } else {
+        let size = metadata.len();
+        total_size += size;
+    }
+    total_size
+}
+
 fn dir_size(dir_path: &Path) -> io::Result<u64> {
     let entries = fs::read_dir(dir_path)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>>>()?;
     let mut total_size: u64 = 0;
     for entry in &entries {
-        let metadata = fs::metadata(entry)?;
-        
-
-        if metadata.is_dir() {
-            let folder_size = dir_size(entry).unwrap_or(0);
-            total_size += folder_size;
-        } else {
-            let size = metadata.len();
-            total_size += size;
-        }
+        total_size += entry_size(entry);
     }
     Ok(total_size)
 }
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-
     let path = Path::new(&args[1]);
     println!("{}", path.display());
 
@@ -49,27 +58,16 @@ fn main() -> io::Result<()> {
 
     let mut entries_by_size: Vec<(u64, &PathBuf)> = Vec::new();
 
-
     for entry in &entries {
-        let metadata = fs::metadata(entry)?;
-
-        if metadata.is_dir() {
-            let folder_size = dir_size(entry).unwrap_or(0);
-            entries_by_size.push((folder_size, entry));
-        } else {
-            let file_size = metadata.len();
-            entries_by_size.push((file_size, entry));
-        }
+        entries_by_size.push((entry_size(entry), entry));
     }
 
-    
     entries_by_size.sort_by_key(|(s, _)| std::cmp::Reverse(*s));
 
     let mut total_size: u64 = 0;
     for (size, path) in &entries_by_size {
         println!("{} ---- {}", path.display(), readable_size(*size));
         total_size += size;
-    
     }
 
     println!("{}", readable_size(total_size));
